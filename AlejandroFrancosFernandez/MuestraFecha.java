68a6d0710a46da4be2f29bc1b44d9c81253c80f2
Alejandro <alejandrofrafer@gmail.com>
2023-11-25 12:05:19 +0100

    Haciendo prueba con Git Bash

---
+++ AlejandroFrancosFernandez/MuestraFecha.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ *
+ * @author fraferal
+ */
+public class MuestraFecha {
+    
+    public static void main(String[] args) {
+        
+         Date hoy=new Date();
+         System.out.println(hoy);
+         
+         LocalDate today=LocalDate.now();
+         System.out.println(today);
+         
+         LocalDateTime now= LocalDateTime.now();
+         System.out.println(now);
+                 
+    }
+}
